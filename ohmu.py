bf2577fb97fa48f3ed38438e08b03638e9147c1a
Paul Nechifor <paul@nechifor.net>
2015-07-19 21:22:15 +0100

    Start to use curses.

--- ohmu.py
+++ ohmu.py
@@ -1,5 +1,65 @@
-import sys
+#!/usr/bin/env python
+
+import curses
+import heapq
+
+
+class File(object):
+    def __init__(self, name, is_dir=False, parent=None, size=0):
+        self.name = name
+        self.is_dir = is_dir
+        self.parent = parent
+        self.size = size
+        self.children = []
+
+    def add_child(self, file):
+        file.parent = self
+        heapq.heappush(self.children, (file.size, file))
+        parent = self
+        while parent:
+            parent.size += file.size
+            parent = parent.parent
+
+    def __iter__(self):
+        return (x[1] for x in reversed(self.children))
+
+
+class FileView(object):
+    def __init__(self, file):
+        self.file = file
+
+    def generate(self):
+        pass
+
+
+class Screen(object):
+    def __init__(self):
+        self.screen = curses.initscr()
+        self.height = -1
+        self.width = -1
+
+    def start(self):
+        curses.noecho()
+        curses.cbreak()
+        self.screen.keypad(1)
+        self.update_size()
+
+    def update_size(self):
+        self.height, self.width = self.screen.getmaxyx()
+
+    def stop(self):
+        curses.nocbreak()
+        self.screen.keypad(0)
+        curses.echo()
+        curses.endwin()
 
 
 def main():
-    print 'asdf'
+    screen = Screen()
+    screen.start()
+    screen.screen.getch()
+    screen.stop()
+
+
+if __name__ == '__main__':
+    main()
--- test.py
+++ test.py
@@ -2,6 +2,27 @@ from unittest import TestCase
 
 import ohmu
 
-class TestOhmu(TestCase):
-    def test_is_working(self):
-        self.assertTrue(True)
+
+class TestFile(TestCase):
+    def test_dirs_have_the_size_of_their_children(self):
+        a = ohmu.File('a', is_dir=True)
+        b = ohmu.File('b', is_dir=True)
+        c = ohmu.File('c', is_dir=True)
+        a.add_child(b)
+        b.add_child(c)
+
+        c.add_child(ohmu.File('d', size=1))
+        c.add_child(ohmu.File('e', size=1))
+        b.add_child(ohmu.File('f', size=2))
+        a.add_child(ohmu.File('e', size=3))
+
+        self.assertEqual(c.size, 2)
+        self.assertEqual(b.size, 4)
+        self.assertEqual(a.size, 7)
+
+    def test_files_are_in_order(self):
+        root = ohmu.File('p', is_dir=True)
+        root.add_child(ohmu.File('a', size=2))
+        root.add_child(ohmu.File('b', size=1))
+        root.add_child(ohmu.File('c', size=3))
+        self.assertEqual([x.name for x in root], ['c', 'a', 'b'])
