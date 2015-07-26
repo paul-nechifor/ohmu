ab504265f083711d9bf7e368d1504584a9dcb15b
Paul Nechifor <paul@nechifor.net>
2015-07-26 21:12:10 +0100

    v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ohmu',
-    version='0.2.0',
+    version='0.2.1',
     author='Paul Nechifor',
     author_email='paul@nechifor.net',
     description='View space usage in your terminal.',
