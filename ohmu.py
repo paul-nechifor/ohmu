514abc7de08f7550eb1def3e360ccf22805e06bd
Paul Nechifor <paul@nechifor.net>
2015-07-26 15:53:55 +0100

    Scan files.

--- ohmu.py
+++ ohmu.py
@@ -1,44 +1,71 @@
 #!/usr/bin/env python
 
-from threading import Thread
-from os.path import abspath
+from os.path import abspath, basename, join
+from stat import S_ISDIR, S_ISREG
+from threading import Thread, RLock
 import curses
-import heapq
+import os
 import sys
 import time
 
 
 class File(object):
-    def __init__(self, name, is_dir=False, parent=None, size=0):
+
+    def __init__(self, name, is_dir=False, parent=None, size=0, path=None):
         self.name = name
         self.is_dir = is_dir
         self.parent = parent
         self.size = size
+        self.path = path
         self.children = []
 
     def add_child(self, file):
         file.parent = self
-        heapq.heappush(self.children, (file.size, file))
+        file.path = join(self.path, file.name)
+        self.children.append(file)
+
         parent = self
         while parent:
             parent.size += file.size
             parent = parent.parent
 
-    def __iter__(self):
-        return (x[1] for x in reversed(self.children))
+    def sortAll(self):
+        self.children.sort(key=lambda x: -x.size)
+        for x in self.children:
+            x.sortAll()
 
 
 class Scanner(Thread):
+
     def __init__(self, root_path):
         super(Scanner, self).__init__()
-        self.root = File(root_path)
+        self.lock = RLock()
         self.finished = False
+        path = abspath(root_path)
+        self.root = File(basename(path), is_dir=True, path=path)
 
     def run(self):
-        pass
+        self.scan(self.root)
+
+    def scan(self, parent):
+        dirs = []
+        with self.lock:
+            for f in os.listdir(parent.path):
+                path = join(parent.path, f)
+                stat = os.stat(path)
+                mode = stat.st_mode
+                if S_ISDIR(mode):
+                    dir = File(f, is_dir=True)
+                    parent.add_child(dir)
+                    dirs.append(dir)
+                elif S_ISREG(mode):
+                    parent.add_child(File(f, size=stat.st_size))
+        for dir in dirs:
+            self.scan(dir)
 
 
 class FileView(object):
+
     def __init__(self, file):
         self.file = file
 
@@ -47,6 +74,7 @@ class FileView(object):
 
 
 class Screen(object):
+
     def __init__(self):
         self.screen = curses.initscr()
         self.height = -1
@@ -74,6 +102,7 @@ class Screen(object):
 
 
 class Ohmu(object):
+
     refresh_rate = 0.05
 
     def __init__(self, root_path):
--- test.py
+++ test.py
@@ -1,13 +1,26 @@
-from unittest import TestCase
+from contextlib import contextmanager
+from os.path import join
+from tempfile import mkdtemp
+from unittest import TestCase as BaseTestCase
+import os
+import shutil
 
 import ohmu
 
 
+class TestCase(BaseTestCase):
+
+    def equalities(self, *args):
+        for a, b in zip(*(iter(args),) * 2):
+            self.assertEqual(a, b)
+
+
 class File(TestCase):
+
     def test_dirs_have_the_size_of_their_children(self):
-        a = ohmu.File('a', is_dir=True)
-        b = ohmu.File('b', is_dir=True)
-        c = ohmu.File('c', is_dir=True)
+        a = ohmu.File('a', is_dir=True, path='/')
+        b = ohmu.File('b', is_dir=True, path='/')
+        c = ohmu.File('c', is_dir=True, path='/')
         a.add_child(b)
         b.add_child(c)
 
@@ -16,13 +29,62 @@ class File(TestCase):
         b.add_child(ohmu.File('f', size=2))
         a.add_child(ohmu.File('e', size=3))
 
+        a.sortAll()
         self.assertEqual(c.size, 2)
         self.assertEqual(b.size, 4)
         self.assertEqual(a.size, 7)
 
     def test_files_are_in_order(self):
-        root = ohmu.File('p', is_dir=True)
+        root = ohmu.File('p', is_dir=True, path='/')
         root.add_child(ohmu.File('a', size=2))
         root.add_child(ohmu.File('b', size=1))
         root.add_child(ohmu.File('c', size=3))
-        self.assertEqual([x.name for x in root], ['c', 'a', 'b'])
+        root.sortAll()
+        self.assertEqual([x.name for x in root.children], ['c', 'a', 'b'])
+
+
+class Scanner(TestCase):
+    def test_read_file_sizes(self):
+        structure = {
+            'd1': {
+                'a': '-' * 10,
+                'b': '-' * 5,
+                'd2': {
+                    'c': '-' * 1,
+                    'd': '-' * 3,
+                },
+            },
+        }
+        with self.file_structure(structure) as dir:
+            scanner = ohmu.Scanner(join(dir, 'd1'))
+            scanner.start()
+            scanner.join()
+
+        root = scanner.root
+        self.equalities(
+            root.name, 'd1',
+            root.is_dir, True,
+            root.parent, None,
+            root.size, 19,
+        )
+        root.sortAll()
+        self.equalities(
+            [(x.name, x.size) for x in root.children],
+            [('a', 10), ('b', 5), ('d2', 4)],
+        )
+
+    @contextmanager
+    def file_structure(self, structure):
+        dir = mkdtemp()
+        self.create_file_structure(structure, dir)
+        yield dir
+        shutil.rmtree(dir)
+
+    def create_file_structure(self, structure, dir):
+        for name, value in structure.items():
+            path = join(dir, name)
+            if isinstance(value, basestring):
+                open(path, 'w').write(value)
+            else:
+                os.mkdir(path)
+                self.create_file_structure(value, path)
